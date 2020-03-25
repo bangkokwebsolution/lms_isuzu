be3f5ad8dafc6358d17e88cb1f2d876dc164f50f
LAPTOP-6BV0T4IK\ADMIN <ponathip>
2020-03-25 18:24:31 +0700

    education

---
+++ protected/models/Education.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * This is the model class for table "mt_education".
+ *
+ * The followings are the available columns in table 'mt_education':
+ * @property integer $edu_id
+ * @property string $edu_name
+ * @property string $created_by
+ * @property string $created_date
+ * @property string $updated_by
+ * @property string $updated_date
+ * @property string $active
+ */
+class Education extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'mt_education';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('edu_name, created_by, updated_by', 'length', 'max'=>255),
+			array('active', 'length', 'max'=>1),
+			array('created_date, updated_date', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('edu_id, edu_name, created_by, created_date, updated_by, updated_date, active', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'edu_id' => 'Edu',
+			'edu_name' => 'Edu Name',
+			'created_by' => 'Created By',
+			'created_date' => 'Created Date',
+			'updated_by' => 'Updated By',
+			'updated_date' => 'Updated Date',
+			'active' => 'Active',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('edu_id',$this->edu_id);
+		$criteria->compare('edu_name',$this->edu_name,true);
+		$criteria->compare('created_by',$this->created_by,true);
+		$criteria->compare('created_date',$this->created_date,true);
+		$criteria->compare('updated_by',$this->updated_by,true);
+		$criteria->compare('updated_date',$this->updated_date,true);
+		$criteria->compare('active',$this->active,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Education the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
