b570d7409904f06cc845213096391ddb6b6b841d
Saharat_Inchaiya <programf07@gmail.com>
2020-03-26 00:13:44 +0700

    counter

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -339,18 +339,19 @@ class SiteController extends Controller
 	{
 		$dateNow  =date("d-m-Y");
 		$ipaddress = '';
-		if ($_SERVER['HTTP_CLIENT_IP'] != '127.0.0.1')
-			$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-		else if ($_SERVER['HTTP_X_FORWARDED_FOR'] != '127.0.0.1')
-			$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-		else if ($_SERVER['HTTP_X_FORWARDED'] != '127.0.0.1')
-			$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-		else if ($_SERVER['HTTP_FORWARDED_FOR'] != '127.0.0.1')
-			$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-		else if ($_SERVER['HTTP_FORWARDED'] != '127.0.0.1')
-			$ipaddress = $_SERVER['HTTP_FORWARDED'];
-		else if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1')
-			$ipaddress = $_SERVER['REMOTE_ADDR'];
+		
+		if (getenv('HTTP_CLIENT_IP'))
+			$ipaddress = getenv('HTTP_CLIENT_IP');
+		else if(getenv('HTTP_X_FORWARDED_FOR'))
+			$ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+		else if(getenv('HTTP_X_FORWARDED'))
+			$ipaddress = getenv('HTTP_X_FORWARDED');
+		else if(getenv('HTTP_FORWARDED_FOR'))
+			$ipaddress = getenv('HTTP_FORWARDED_FOR');
+		else if(getenv('HTTP_FORWARDED'))
+			$ipaddress = getenv('HTTP_FORWARDED');
+		else if(getenv('REMOTE_ADDR'))
+			$ipaddress = getenv('REMOTE_ADDR');
 		else
 			$ipaddress = 'UNKNOWN';
 
