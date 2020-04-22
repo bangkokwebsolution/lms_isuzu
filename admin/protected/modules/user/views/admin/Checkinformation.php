52756612e81ab3e7b73f0b64d4fa50dd85ac75ac
server77 <earthgame1355@gmail.com>
2020-04-22 15:47:16 +0700

    fix

--- admin/protected/modules/user/views/admin/Checkinformation.php
+++ admin/protected/modules/user/views/admin/Checkinformation.php
@@ -1,19 +1,131 @@
-<html>
-<head>
-    <title></title>
-    <style type="text/css">
+
+<style type="text/css">
+	.topic-info{
+		color: #000;
+	}
+	.profile-detail h5{
+		margin: 8px 0;
+	}
+	.profile-detail h5 b{
+		color: #333;
+	    padding-right: 8px;
+	    min-width: 120px;
+	    display: inline-block;
+	    text-align: right;
+	}
+	.profile-detail h5 span{
+		color: #333;
+		padding: 0 8px;
+	}
+	.profile-detail h5 ul{
+		list-style: square;
+	}
+	.profile-detail h5 ul li a span{
+		color: blue !important;
+	}
+	.mt-1{
+		margin-top: 1em;
+	}
+	.mt-2{
+		margin-top: 2em;
+	}
+
+	.d-inlineblock{
+		display: inline-block;
+		width: auto !important;
+	}
 </style>
-</head>
-<body>
+
 <form>
-	<div class="well">
-<?php
+	<div class="well profile-detail">
+
+		<h4 class="topic-info">ข้อมูลพื้นฐาน</h4>
+		<div class="mt-1">
+			<div class="row">
+				<div class="col-md-12"><h5><b>ชื่อ-นามสกุล:</b><span>นาย&nbsp;&nbsp;ชื่อเต็ม&nbsp;&nbsp;นามสกุลเต็ม</span></h5></div>
+				<div class="col-md-12"><h5><b>Name:</b><span>mr.&nbsp;&nbsp;firstname&nbsp;&nbsp;lastname</span></h5></div>
+				<div class="col-md-6"><h5><b>เลขบัตรประจำตัวประชาชน:</b><span>xxxxxxxxxxxx</span> </h5></div>
+				<div class="col-md-6"><h5><b>วันที่บัตรหมดอายุ:	</b><span>10/08/2560</span></h5></div>
+				<div class="col-md-6"><h5><b>วันเดือนปีเกิด:</b><span>20/05/2535</span> </h5></div>
+				<div class="col-md-6"><h5><b>อายุ:</b><span>26</span></h5></div>
+				<div class="col-md-6"><h5><b>เชื้อชาติ:</b><span>xxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>สัญชาติ:</b><span>xxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>ศาสนา:</b><span>xxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>เพศ:</b><span>xxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>สถานะภาพทางการสมรส:</b><span>โสด</span></h5></div>
+				<div class="col-md-12"><h5><b>ที่อยู่:</b><span>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>เบอร์โทรศัพท์:</b><span>xxxxxxxxxxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>อีเมล:</b><span>thoresen@mail.com</span></h5></div>
+				<div class="col-md-12"><h5><b>ไอดีไลน์:</b><span>xxxxxxxxx</span></h5></div>
+				<div class="col-md-6"><h5><b>ประวัติการเจ็บป่วยรุนแรง:</b><span>xxxxxxxxx</span></h5></div>
+				<div class="col-md-12">
+					<h5><b>ประวัติการศึกษา:</b>
+						<ul>
+							<li>
+								<span>ระดับการศึกษา</span><span>สถานที่จบการศึกษา</span><span>ปีที่จบการศึกษา</span>
+							</li>
+							<li>
+								<span>ระดับการศึกษา</span><span>สถานที่จบการศึกษา</span><span>ปีที่จบการศึกษา</span>
+							</li>
+						</ul>
+					</h5>
+				</div>
+				<div class="col-md-12">
+					<h5><b>เอกสารแนบไฟล์วุฒิการศึกษา/วิชาชีพ:</b>
+						<ul>
+							<li>
+								<a href="#"><span>ไฟล์</span></a>
+							</li>
+							<li>
+								<a href="#"><span>ไฟล์</span></a>
+							</li>
+						</ul>
+					</h5>
+				</div>
+				<div class="col-md-12">
+					<h5><b>เอกสารแนบไฟล์ฝึกอบรม:</b>
+						<ul>
+							<li>
+								<a href="#"><span>ไฟล์</span></a>
+							</li>
+							<li>
+								<a href="#"><span>ไฟล์</span></a>
+							</li>
+						</ul>
+					</h5>
+				</div>
+				<div class="col-md-12">
+					<h5><b>ตำแหน่งเรือที่ท่านสนใจสมัคร:</b>
+					<select class="form-control d-inlineblock" name="]" id="">
+						<option value="">เลือกตำแหน่ง</option>
+						<option value="1">C/O</option>
+						<option value="2">2/O</option>
+						<option value="3">3/O</option>
+						<option value="4">Bosun</option>
+						<option value="5">AB</option>
+						<option value="6">D/Boy</option>
+						<option value="7">D/Boy1</option>
+						<option value="8">C/E</option>
+						<option value="9">2/E</option>
+						<option value="10">3/E</option>
+						<option value="11">4/E</option>
+						<option value="12">Fitter</option>
+						<option value="13">Oiler</option>
+						<option value="14">E/Boy</option>
+						<option value="15">E/Boy1</option>
+					</select>
+				</h5>
+				</div>
+			</div>
+			<div class="text-center mt-2">
+                 <button class="btn btn-primary btn-icon "><i></i>บันทึกข้อมูล</button>                        
+             </div>
+		</div>
+<!-- <?php
 
 echo $user["email"];
-?>
-
-             
-        </div>
-    </form>
+?> -->	
+         
+    </div>
+</form>
 </body>
-</html>
--- admin/protected/modules/user/views/admin/membership.php
+++ admin/protected/modules/user/views/admin/membership.php
@@ -192,8 +192,8 @@ Yii::app()->clientScript->registerScript('search', "
 							    <div class="modal-dialog modal-lg" role="document">
 							        <div class="modal-content">
 							            <div class="modal-header" style="background-color: #3C8DBC;">
-							                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							                <h4 class="modal-title" style="font-size: 25px;color: #fff;}">ข้อความ</h4>
+							                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="color: #fff;font-size: 22px;">&times;</span></button>
+							                <h4 class="modal-title" style="font-size: 20px;color: #fff;padding: .3em;">ข้อความ</h4>
 							            </div>
 							            <div class="modal-body">
 							            </div>
