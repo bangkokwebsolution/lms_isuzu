58dc1f0067b4131e067b55c3bf95256c7d1ec35e
nooknik <laksika.prk@gmail.com>
2020-05-25 12:26:22 +0700

    หลังบ้าน course online toggle

--- admin/protected/views/courseOnline/_form.php
+++ admin/protected/views/courseOnline/_form.php
@@ -16,8 +16,7 @@
 
 </script> -->
 <!-- innerLR -->
-<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+
 <style>
 	<style>
 .checkbox label:after {
