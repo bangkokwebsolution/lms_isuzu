c12569e13ce17553726295eca288514ed819d103
LAPTOP-6BV0T4IK\ADMIN <ponathip>
2020-08-13 09:30:02 +0700

    mail

---
+++ protected/views/registration/Form_mail_Ship.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+  <title></title>
+  <style type="text/css">
+</style>
+</head>
+
+<body>
+
+  <h3><u>สมัครสมาชิกสำเร็จ\ Registered successfully</u></h3>
+  <h4>เรียน : <?= $firstname.' '.$lastname; ?></h4>
+  <h4>ท่านได้ทำการสมัครเพื่อเข้าใช้บริการ thorconn.com และแอพพลิเคชั่น Thoresen e-Leaning เรียบร้อยแล้ว </h4>
+  <h4>กรุณารอการตรวจสอบข้อมูลจากระบบเพื่อรอรับ ชื่อผู้ใช้และรหัสผ่าน ในการเข้าใช้งาน</h4>
+  <br><br>
+  <h3><u>Registered successfully</u></h3>
+  <h4>เรียน : <?= $firstname_en.' '.$lastname_en; ?></h4>
+  <h4>You have successfully applied a member of thorconn.com and Thoresen e-Leaning application.</h4>
+  <h4>Please wait to system verify your personal data to be received your Username & Password.</h4>
+
+</body>
+</html>
