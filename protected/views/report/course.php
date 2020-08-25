c89783e7c2449f25fd523b549efe653809351ac4
nooknik <laksika.prk@gmail.com>
2020-08-25 14:48:03 +0700

    หน้าบ้าน report

--- protected/views/report/course.php
+++ protected/views/report/course.php
@@ -413,7 +413,7 @@ if(isset($model_level) && !empty($model_level)){
                                         ?>
                                         </option>
                                         <?php 
-                                        for ($i=($year_start+1); $i<=$year_end ; $i++) {
+                                        for ($i=($year_start+1); $i<$year_end ; $i++) {
                                             ?> <option <?php if(isset($_GET["search"]["end_year"]) && $_GET["search"]["end_year"] == $i){ echo "selected"; } ?> value="<?= $i ?>"><?= $i ?></option> <?php
                                         }
                                          ?>
