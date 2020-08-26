b4342c668ca69ff7d4e4646bfca03ac71a3f65b8
Oil-Aranya <aranyathongyoy@gmail.com>
2020-08-26 16:34:25 +0700

    แก้ข้อความ

--- admin/protected/views/Document/update_type.php
+++ admin/protected/views/Document/update_type.php
@@ -1,12 +1,12 @@
 
 <?php
 $this->breadcrumbs=array(
-	'ระบบประเภทอกสาร'=>array('index'),
-	'แก้ไขประเภทอกสาร',
+	'ระบบประเภทเอกสาร'=>array('index'),
+	'แก้ไขประเภทเอกสาร',
 );
 ?>
 <?php echo $this->renderPartial('form_type', array(
 	'model'=>$model,
 	'file'=>$file,
-	'formtext'=>'แก้ไขเประเภทอกสาร'
+	'formtext'=>'แก้ไขเประเภทเอกสาร'
 )); ?>
