c6a99de6d4cf790ac9ee9d77313386078eeac40e
p <>
2021-09-07 18:09:18 +0700

    Approve

--- admin/protected/controllers/ApproveCourseController.php
+++ admin/protected/controllers/ApproveCourseController.php
@@ -61,14 +61,23 @@ class ApproveCourseController extends Controller
 
 			$form_text = "";
 			if ($CourseOnline != "") {
-				$form_text .= "<h3 style='margin-bottom: 10px;'><b>ข้อมูลหลักสูตร</b></h3>";
+				$form_text .= "<h3 style='margin-bottom: 10px;'><b>ข้อมูล</b></h3>";
 
 				$form_text .= "<div class='row' style='margin-bottom: 20px;'>";
 				$form_text .= "<div class='col-md-12'>";
-				$form_text .= "<p class='pull-left' style='display: inline-block; font-size: 16px;'>Course Name</p>";
+				$form_text .= "<p class='pull-left' style='display: inline-block; font-size: 16px;'>หลักสูตร</p>";
 				$form_text .= "</div>";
 				$form_text .= "<div class='col-md-6'>";
 				$form_text .= "<input class='form-control' style='height: 40px;' readonly type='text' value='" . $CourseOnline->course_title . "'>";				
+				$form_text .= "</div>";
+				$form_text .= "</div>";
+
+				$form_text .= "<div class='row' style='margin-bottom: 20px;'>";
+				$form_text .= "<div class='col-md-12'>";
+				$form_text .= "<p class='pull-left' style='display: inline-block; font-size: 16px;'>หมวด</p>";
+				$form_text .= "</div>";
+				$form_text .= "<div class='col-md-6'>";
+				$form_text .= "<input class='form-control' style='height: 40px;' readonly type='text' value='" . $CourseOnline->cates->cate_title . "'>";				
 				$form_text .= "</div>";
 				$form_text .= "</div>";
 			}
