3066d4903cd1bbf7d2184b580e9dcb8c713fb24a
LAPTOP-6BV0T4IK\ADMIN <ponathip>
2021-09-23 00:18:01 +0700

    fix courseplan css

--- protected/views/course/courseplan.php
+++ protected/views/course/courseplan.php
@@ -87,7 +87,7 @@ if (empty(Yii::app()->session['lang']) || Yii::app()->session['lang'] == 1) {
                             <div class="cell" style="grid-row:<?= $row ?>;"></div>
                             <div class="cell" style="grid-row:<?= $row ?>;"></div>
                             <div class="cell" style="grid-row:<?= $row ?>;"></div>
-                            <section class="event row-plan<?= $row ?>" style=" grid-column: <?= $date_start[1]+1 ?> / span <?= $date_end[1]-$date_start[1]+1 ?>;background-color: <?= $status_user ?>"> <?= $date_Course ?></section>
+                            <section class="event row-plan<?= $row ?>" style="grid-row:<?= $row ?>; grid-column: <?= $date_start[1]+1 ?> / span <?= $date_end[1]-$date_start[1]+1 ?>;background-color: <?= $status_user ?>"> <?= $date_Course ?></section>
                     <?php 
 
                                 $row++;
