10c21b9d010acc7a97f90ac3aef2ff9e1284b68f
Tai <chayuttatat@gmail.com>
2021-10-25 14:46:03 +0700

    fix login เพิ่ม query

--- protected/views/site/loginform.php
+++ protected/views/site/loginform.php
@@ -10,34 +10,43 @@
         <div class="login-group row justify-content-center align-items-center">
             <div class="col-sm-6 col-md-5 text-center">
                 <div class="logo-head">
-                <a href="">
-                    <img src="../themes/template2/images/logo-imct.png" width="250px" class="logo-login">
-                </a>
+                    <a href="">
+                        <img src="../themes/template2/images/logo-imct.png" width="250px" class="logo-login">
+                    </a>
                 </div>
                 <div class="login-content">
                     <div class="login-form">
                         <h3> เข้าสู่ระบบ </h3>
-                        <form action="">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Username">
-                            </div>
-                        </form>
-                        <form action="">
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Password">
-                            </div>
-                        </form>
+                        <div class="form-group">
+                            <label for=""><?= $label->label_header_username ?></label>
+                            <input type="text" class="form-control" placeholder='<?= $label->label_header_username ?>' name="UserLogin[username]" value="<?php echo Yii::app()->request->cookies['cookie_name']->value; ?>" required>
+                        </div>
+
+
+                        <div class="form-group password-group">
+                            <label for=""><?= $label->label_header_password ?></label>
+                            <input type="password" id="password-field" class="form-control" placeholder='<?= $label->label_header_password ?>' name="UserLogin[password]" required>
+                            <span toggle="#password-field" class="field-icon toggle-password"></span>
+                            <!-- <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span> -->
+                        </div>
+
                         <div class="login-btn">
-                            <button type="submit" class="btn btn-submit login-main" id="submit" name="submit">Login</button>
+                            <button type="submit" class="btn btn-submit login-main" id="submit" name="submit"><?= $label->label_header_yes ?>Login</button>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <a href="" class="mt-1 pull-right"> ลืมรหัสผ่าน</a>
-                            </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <span class="pull-right" style="margin-top: 5px">
+                                <a class="btn-forgot" href="<?php echo $this->createUrl('Forgot_password/index') ?>"><?= $label->label_header_forgotPass ?>ลืมรหัสผ่าน</a>
+                                <!-- <a href="< ?php echo $this->createUrl('/registration/ShowForm'); ?>"><i class="fa fa-user-plus" aria-hidden="true"></i> <?= $label->label_header_regis ?></a> -->
+                            </span>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </body>
