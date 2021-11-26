b30e204189d2e05ffe0d4a1884b1131a9490cfb6
p <>
2021-11-26 19:24:45 +0700

    cer

--- protected/views/course/cerfile/Newcertificate.php
+++ protected/views/course/cerfile/Newcertificate.php
@@ -39,15 +39,15 @@
     .red { color: #d05951; }
 </style>
 
- <div style="position:absolute; top: 10mm;text-align: center;" ><img width="140mm" src="../admin/images/cer_1.png" ></div>
+ <div style="position:absolute; top: 10mm;text-align: center;" ><img width="140mm" src="admin/images/cer_1.png" ></div>
 
  <div class="b size30 " style="position:absolute; top: 80mm;">
     Mr.Tester  Tester
 </div>
 
-<div style="position:absolute; top: 100mm;" ><img width="90mm" src="../admin/images/cer_2.png" ></div>
+<div style="position:absolute; top: 100mm;" ><img width="90mm" src="../../admin/images/cer_2.png" ></div>
 
-<div style="position:absolute; top: 10mm;left: 130mm " ><img width="50mm" src="../admin/images/cer_3.png" ></div>
+<div style="position:absolute; top: 10mm;left: 130mm " ><img width="50mm" src="../../../admin/images/cer_3.png" ></div>
 
 <!-- padding-left:610px; -->
 
