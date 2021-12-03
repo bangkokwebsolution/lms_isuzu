5006ab622a5d3ffbb1232c47cfc05254011bc8ea
p <>
2021-12-03 15:49:29 +0700

    wording

--- admin/protected/views/Document/create.php
+++ admin/protected/views/Document/create.php
@@ -1,12 +1,12 @@
 
 <?php
 $this->breadcrumbs=array(
-	'ระบบหมวดเอกสาร'=>array('index'),
+	'ระบบเอกสาร'=>array('index'),
 	'เพิ่มเอกสาร',
 );
 ?>
 <?php echo $this->renderPartial('_form', array(
 	'model'=>$model,
 	'file'=>$file,
-	'formtext'=>'เพิ่มหมวดเอกสาร'
+	'formtext'=>'เพิ่มเอกสาร'
 )); ?>
