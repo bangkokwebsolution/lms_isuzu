4cdb22a55ca1c54cd5a3fa689ef65df9d7905b66
p <>
2021-12-20 01:21:15 +0700

    slide

--- protected/controllers/CourseController.php
+++ protected/controllers/CourseController.php
@@ -1010,10 +1010,10 @@ public function actionCateIndex($id) {
                     $index =  $slide - 1;
                     $att['no']      = $id;
                     // $att['timeNext'] = $filePdfSlide->image_slide_next_time;
-                    // if($index%5 == 0 && $slide != 0 && $modelLearnFilePdf->learn_file_status != $slide){
+                    if($index%5 == 0 && $slide != 0 && $modelLearnFilePdf->learn_file_status != $slide){
                       
                         $att['indicators'] = '<li data-target="#myCarousel'.$id.'" data-slide-to="'.$index.'" >'.$index.'</li>';
-                    // }
+                    }
                     $modelLearnFilePdf->learn_file_status = $slide;
                     
                 }else{
