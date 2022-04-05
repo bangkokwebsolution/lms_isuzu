03a390dae20fe35926cb00a539671d52cd4e134d
DESKTOP-IH4968T\llOzakall <kengker1144@gmail.com>
2022-04-05 16:52:37 +0700

    report 4

--- admin/protected/views/report/ExcelByCourseAll.php
+++ admin/protected/views/report/ExcelByCourseAll.php
@@ -9,22 +9,20 @@ header("Content-Type: application/octet-stream");
 header("Content-Type: application/download");
 header("Pragma:no-cache");
 
-if(isset($_GET['Report']['course_id']) && $_GET['Report']['course_id'] !=''){
-        $search = $_GET['Report'];
-        
-        $criteria = New CDbCriteria;
-        if(isset($_GET['Report']['course_id']) && $_GET['Report']['course_id'] != null) {
-            $criteria->compare('t.course_id', $_GET['Report']['course_id']);
-        }
+if (isset($_GET['Report']['course_id']) && $_GET['Report']['course_id'] != '') {
+    $search = $_GET['Report'];
 
-        if(isset($_GET['Report']['gen_id']) && $_GET['Report']['gen_id'] != null) {
-            $criteria->compare('t.gen_id', $_GET['Report']['gen_id']);
-        }
-        $course_online = CourseOnline::model()->findByPk($search['course_id']);
+    $criteria = new CDbCriteria;
+    if (isset($_GET['Report']['course_id']) && $_GET['Report']['course_id'] != null) {
+        $criteria->compare('t.course_id', $_GET['Report']['course_id']);
+    }
 
-        $user_Learn = LogStartcourse::model()->findAll($criteria);
+    if (isset($_GET['Report']['gen_id']) && $_GET['Report']['gen_id'] != null) {
+        $criteria->compare('t.gen_id', $_GET['Report']['gen_id']);
+    }
+    $course_online = CourseOnline::model()->findByPk($search['course_id']);
 
-
+    $user_Learn = LogStartcourse::model()->findAll($criteria);
 }
 ?>
 <style type="text/css">
@@ -42,69 +40,134 @@ if(isset($_GET['Report']['course_id']) && $_GET['Report']['course_id'] !=''){
     <div class="widget-body" style=" overflow-x: scroll;">
         <table class="table table-bordered table-striped" border='1'>
             <thead>
-              <tr>
-                <th  style="vertical-align: middle;" class="center"><b>No.</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Minor course type</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Subminor course type</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Course Name</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Gen</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Group</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Employee</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Name</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Organization unit</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Abbreviate code</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Employee class</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Position desc.</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Learning Status</b></th>
-                <th  style="vertical-align: middle;" class="center"><b>Completed Date</b></th>
-            </tr>
-                    </thead>
-                    <tbody>
-                        <?php $no =1; 
-                    foreach ($user_Learn as $keyL => $valueL) {
-                        if (!isset($valueL->user)) {
-                            continue;
-                        }
-                        // if(isset($search['gen_id']) && $search['gen_id'] !=''){
-                        //     if($search['gen_id'] != $valueL->course->getGenID($valueL->course_id)){ continue; }
-                        // }
-                        $status_course_gen = Helpers::lib()->StatusCourseGen($valueL->course_id, $valueL->course->getGenID($valueL->course_id),$valueL->user_id);
+                <tr>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>No.</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Minor course type</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Subminor course type</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Course Name</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Gen</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Group</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Employee</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Name</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Organization unit</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Abbreviate code</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Employee class</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Position desc.</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Learning Status</b></th>
+                    <th rowspan="2" style="vertical-align: middle;" class="center"><b>Completed Date</b></th>
+                    <th colspan="4">Final Test</th>
+                </tr>
+                <tr>
+                    <th style="vertical-align: middle;" class="center">Score</th>
+                    <th style="vertical-align: middle;" class="center">Percent</th>
+                    <th style="vertical-align: middle;" class="center">Final Test Status</th>
+                    <th style="vertical-align: middle;" class="center">Evaluation</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $no = 1;
+                foreach ($user_Learn as $keyL => $valueL) {
+                    if (!isset($valueL->user)) {
+                        continue;
+                    }
+                    // if(isset($search['gen_id']) && $search['gen_id'] !=''){
+                    //     if($search['gen_id'] != $valueL->course->getGenID($valueL->course_id)){ continue; }
+                    // }
+                    $status_course_gen = Helpers::lib()->StatusCourseGen($valueL->course_id, $valueL->course->getGenID($valueL->course_id), $valueL->user_id);
 
-                        if ($status_course_gen == "notLearn") {
-                            $lessonStatusStr = 'Not start';
-                        } else if ($status_course_gen == "learning") {
-                            $lessonStatusStr = 'On process';
-                        } else if ($status_course_gen == "pass") {
-                            $lessonStatusStr =  'Completed';
-                        }
+                    if ($status_course_gen == "notLearn") {
+                        $lessonStatusStr = 'Not start';
+                    } else if ($status_course_gen == "learning") {
+                        $lessonStatusStr = 'On process';
+                    } else if ($status_course_gen == "pass") {
+                        $lessonStatusStr =  'Completed';
+                    }
 
-                        $passcourse = Passcours::model()->find(array( 
-                            'condition' => 'gen_id=:gen_id AND passcours_cours=:course_id AND passcours_user=:user_id',
-                            'params' => array(':gen_id'=>$valueL->course->getGenID($valueL->course_id), ':course_id'=>$valueL->course_id, ':user_id'=>$valueL->user_id),
+                    $passcourse = Passcours::model()->find(array(
+                        'condition' => 'gen_id=:gen_id AND passcours_cours=:course_id AND passcours_user=:user_id',
+                        'params' => array(':gen_id' => $valueL->course->getGenID($valueL->course_id), ':course_id' => $valueL->course_id, ':user_id' => $valueL->user_id),
+                    ));
+                    //---------------คะแนนหลักสูตร-----------------//
+                    $Evaluation = "Pending";
+                    $header_id = Helpers::lib()->chk_course_questionnaire($valueLog->course_id);
+
+                    if ($passcourse != null) {
+                        if ($header_id != null) {
+                            if (Helpers::lib()->chk_course_questionnaire_do($header_id, $valueL->course_id, $passcourse->passcours_user, "")) {
+                                // ทำแบบสอบถามแล้ว
+                                $Evaluation = "Completed";
+                                $Training_Status = "Completed";
+                                $percent_course = "100";
+                                $Completed_date = date("d-M-Y", strtotime($passcourse->passcours_date));
+                            }
+                        } else {
+                            $Training_Status = "Completed";
+                            $percent_course = "100";
+                            $Completed_date = date("d-M-Y", strtotime($passcourse->passcours_date));
+                        }
+                    }
+
+
+                    $course_post_score = "ไม่มีข้อสอบหลักสูตร";
+                    $course_post_percent = "ไม่มีข้อสอบหลักสูตร";
+                    $course_post_status = "ไม่มีข้อสอบหลักสูตร";
+
+                    $manage_course_post = Coursemanage::model()->findAll(array(
+                        'select' => 'group_id',
+                        'condition' => 'id="' . $valueL->course_id . '" AND active="y" AND type="course" ',
+                        'order' => 'manage_id ASC'
+                    ));
+
+                    if (!empty($manage_course_post)) {
+                        $course_post_score = "Not Start";
+                        $course_post_percent = "Not Start";
+                        $course_post_status = "Not Start";
+
+
+                        $coursescore_post = Coursescore::model()->find(array(
+                            'select' => 'score_number, score_total, score_past,course_id',
+                            'condition' => 'type="post" AND course_id="' . $valueL->course_id . '" AND user_id="' . $valueL->user_id . '" AND active="y" ',
+                            'order' => 'score_id DESC'
                         ));
-                     ?>
-                        <tr>
-                            <td class="center"><?= $no++ ?></td>
-                            <td class="center"><?= isset($valueL->course->cates->type->type_name)? $valueL->course->cates->type->type_name :"" ?></td>
-                            <td class="center"><?= $valueL->course->course_number ?></td>
-                            <td><?= $valueL->course->course_title ?></td>
-                            <td class="center"><?= isset($valueL->gen->gen_title) ? $valueL->gen->gen_title : '-' ?></td>
-                            <td class="center"><?= $valueL->pro->group_name ?></td>
-                            <td class="center"><?= $valueL->mem->employee_id ?></td>
-                            <td class="center"><?= $valueL->pro->fullname ?></td>
-                            <td class="center"><?= $valueL->pro->organization_unit ?></td>
-                            <td class="center"><?= $valueL->mem->orgchart->title ?></td>
-                            <td class="center"><?= $valueL->pro->EmpClass->title ?></td>
-                            <td class="center"><?= $valueL->pro->position_description ?></td>
-                            <td class="center"><?= $lessonStatusStr ?></td>
-                            <td class="center"><?= isset($passcourse)? $passcourse->passcours_date :'' ?></td>
-                        </tr>
-                    <?php 
+
+                        if ($coursescore_post) {
+                            $course_post_score = $coursescore_post->score_number . "/" . $coursescore_post->score_total;
+
+                            $course_post_percent = number_format(($coursescore_post->score_number * 100) / $coursescore_post->score_total, 2) . " %";
+
+                            if ($coursescore_post->score_past == "y") {
+                                $course_post_status = "Passed";
+                            } else {
+                                $course_post_status = "Not Passed";
+                            }
+                        }
+                    }
+                ?>
+                    <tr>
+                        <td class="center"><?= $no++ ?></td>
+                        <td class="center"><?= isset($valueL->course->cates->type->type_name) ? $valueL->course->cates->type->type_name : "" ?></td>
+                        <td class="center"><?= $valueL->course->course_number ?></td>
+                        <td><?= $valueL->course->course_title ?></td>
+                        <td class="center"><?= isset($valueL->gen->gen_title) ? $valueL->gen->gen_title : '-' ?></td>
+                        <td class="center"><?= $valueL->pro->group_name ?></td>
+                        <td class="center"><?= $valueL->mem->employee_id ?></td>
+                        <td class="center"><?= $valueL->pro->fullname ?></td>
+                        <td class="center"><?= $valueL->pro->organization_unit ?></td>
+                        <td class="center"><?= $valueL->mem->orgchart->title ?></td>
+                        <td class="center"><?= $valueL->pro->EmpClass->title ?></td>
+                        <td class="center"><?= $valueL->pro->position_description ?></td>
+                        <td class="center"><?= $lessonStatusStr ?></td>
+                        <td class="center"><?= isset($passcourse) ? $passcourse->passcours_date : '' ?></td>
+                        <td class="center"><?= $course_post_score ?>&nbsp;</td>
+                        <td class="center"><?= $course_post_percent ?></td>
+                        <td class="center"><?= $course_post_status ?></td>
+                        <td class="center"><?= $Evaluation ?></td>
+                    </tr>
+                <?php
 
                 } ?>
-                    </tbody>
-                </table>
-                
-            </div>
-        </div>
+            </tbody>
+        </table>
 
+    </div>
+</div>
