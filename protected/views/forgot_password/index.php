5a1626525c5cc99fabd70060e9ebb452c4dbfc27
bws_keng <kengker1144@gmail.com>
2022-04-28 11:44:35 +0700

    add wording

--- protected/views/forgot_password/index.php
+++ protected/views/forgot_password/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include "themes/template2/include/css.php";
 if (empty(Yii::app()->session['lang']) || Yii::app()->session['lang'] == 1) {
     $langId = Yii::app()->session['lang'] = 1;
@@ -7,7 +7,7 @@ if (empty(Yii::app()->session['lang']) || Yii::app()->session['lang'] == 1) {
     $cancel = 'Cancel';
     $ok = 'OK';
     $warn = 'Warning';
-    $note='* Remark : For user who do not have both company e-mail or private e-mail for reset password please contact system administrator at ext.no 1210 or 1211';
+    $note = '* Remark : For user who do not have both company e-mail or private e-mail for reset password please contact system administrator at ext.no 1210 or 1211';
 } else {
     $mail = 'อีเมล';
     $warn = 'แจ้งเตือน';
@@ -15,22 +15,22 @@ if (empty(Yii::app()->session['lang']) || Yii::app()->session['lang'] == 1) {
     $ok = 'ตกลง';
     $cancel = 'ยกเลิก';
     $langId = Yii::app()->session['lang'];
-    $note='* หมายเหตุ : สำหรับผู้ใช้ที่ไม่มีทั้งอีเมลบริษัทหรืออีเมลส่วนตัวสำหรับรีเซ็ตรหัสผ่าน โปรดติดต่อผู้ดูแลระบบที่เบอร์ต่อ 1210 หรือ 1211';
-}  
+    $note = '* หมายเหตุ : สำหรับผู้ใช้ที่ไม่มีทั้งอีเมลบริษัทหรืออีเมลส่วนตัวสำหรับรีเซ็ตรหัสผ่าน โปรดติดต่อผู้ดูแลระบบที่เบอร์ต่อ 1210 หรือ 1211';
+}
 
 if (Yii::app()->user->hasFlash('msg')) {  ?>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script type="text/javascript">
         swal({
-          title: "<?= $warn ?>",
-          text: "<?= $text_email ?>",
-          icon: "warning",
-          buttons: ['<?= $cancel ?>','<?= $ok ?>'],
-          dangerMode: true,
-      })  
-  </script>
-  <?php 
-  Yii::app()->user->setFlash('msg',null);
+            title: "<?= $warn ?>",
+            text: "<?= $text_email ?>",
+            icon: "warning",
+            buttons: ['<?= $cancel ?>', '<?= $ok ?>'],
+            dangerMode: true,
+        })
+    </script>
+<?php
+    Yii::app()->user->setFlash('msg', null);
 }
 
 ?>
@@ -51,21 +51,21 @@ if (Yii::app()->user->hasFlash('msg')) {  ?>
 <body class="body-login">
 
 
-<?php
-if ($_GET["msg"]=="error") { 
-?>
+    <?php
+    if ($_GET["msg"] == "error") {
+    ?>
 
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  <script type="text/javascript">
-    swal({
-      title: "<?= $warn ?>",
-          text: "<?= $text_email ?>",
-          icon: "warning",
-          // buttons: ['<?= $ok ?>'],
-          dangerMode: true,
-    });
-  </script>
-<?php } ?>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script type="text/javascript">
+            swal({
+                title: "<?= $warn ?>",
+                text: "<?= $text_email ?>",
+                icon: "warning",
+                // buttons: ['<?= $ok ?>'],
+                dangerMode: true,
+            });
+        </script>
+    <?php } ?>
 
     <div class="container">
         <div class="login-group row justify-content-center align-items-center">
@@ -77,8 +77,8 @@ if ($_GET["msg"]=="error") {
             <section class="content" id="contact-us">
                 <div class="container">
                     <?php
-                    $form = $this->beginWidget('CActiveForm',array(
-                        'action'=>Yii::app()->createUrl('/forgot_password/Sendpassword')
+                    $form = $this->beginWidget('CActiveForm', array(
+                        'action' => Yii::app()->createUrl('/forgot_password/Sendpassword')
                     ));
                     ?>
                     <div class="well">
@@ -86,19 +86,31 @@ if ($_GET["msg"]=="error") {
                             <div class="col-sm-6 col-sm-offset-3">
                                 <div class="form-group">
                                     <label for=""><?= $mail ?></label><br>
-                                    <?php echo $form->textField($model, 'email', array('class' => 'form-control input-lg' , 'placeholder' => 'example@gmail.com' , 'required' => true)); ?>
+                                    <?php echo $form->textField($model, 'email', array('class' => 'form-control input-lg', 'placeholder' => 'example@gmail.com', 'required' => true)); ?>
                                     <?php echo $form->error($model, 'email'); ?>
                                 </div>
 
                             </div>
-                        </div>         
+                        </div>
+                        <div class="text-center"><b>
+                            Please fill out company e-mail or private e-mail that user register for reset password
+                            <br>
+
+                            (In case user who have not company e-mail)
+                            <br>
+                            กรุณาระบุอีเมลบริษัท หรืออีเมลส่วนตัวที่แจ้งลงทะเบียนไว้เพื่อใช้ในการรีเซ็ตรหัสผ่าน (กรณีผู้ใช้ไม่มีอีเมลบริษัท)
+                            </b>
+                        </div>
+                        <br>
                         <div class="text-center">
                             <!--<button type="submit" class="btn btn-warning btn-lg">ส่งข้อความ</button>-->
-                            <?php echo CHtml::submitButton( $label->label_button , array('class' => 'btn btn-warning btn-lg')); ?>
-                        <br>    
-                        <br>
-                        <br>
-                            <label for="" style="padding-left: 40px;padding-right: 40px"><font color="red">* Remark : For user who do not have both company e-mail or private e-mail for reset password please contact system administrator at ext.no 1210 or 1211 <br><br>* หมายเหตุ : สำหรับผู้ใช้ที่ไม่มีทั้งอีเมลบริษัทหรืออีเมลส่วนตัวสำหรับรีเซ็ตรหัสผ่าน โปรดติดต่อผู้ดูแลระบบที่เบอร์ต่อ 1210 หรือ 1211</font></label>
+                            <?php echo CHtml::submitButton($label->label_button, array('class' => 'btn btn-warning btn-lg')); ?>
+                            <br>
+                            <br>
+                            <br>
+                            <label for="" style="padding-left: 40px;padding-right: 40px">
+                                <font color="red">* Remark : For user who do not have both company e-mail or private e-mail for reset password please contact system administrator at ext.no 1210 or 1211 <br><br>* หมายเหตุ : สำหรับผู้ใช้ที่ไม่มีทั้งอีเมลบริษัทหรืออีเมลส่วนตัวสำหรับรีเซ็ตรหัสผ่าน โปรดติดต่อผู้ดูแลระบบที่เบอร์ต่อ 1210 หรือ 1211</font>
+                            </label>
                             <br>
                         </div>
                     </div>
@@ -109,5 +121,5 @@ if ($_GET["msg"]=="error") {
             </section>
         </div>
     </div>
-</div>
+    </div>
 </body>
