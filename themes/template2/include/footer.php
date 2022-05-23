351ba812009eed8405d76cedd1bfb0614702502f
p <>
2022-05-23 11:34:16 +0700

    wording

--- themes/template2/include/footer.php
+++ themes/template2/include/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
 	<div class="pt-2 pb-2 text-gray">
 		<div class="container">
-			<small class="text-white copyright"> Copyright <span class="sans-font">©</span> 2021 ISUZU Motors (Thailand) Co., Ltd.</small>
+			<small class="text-white copyright"> Copyright <span class="sans-font">©</span> 2021 Isuzu Motors Co., (Thailand) Ltd.</small>
 			<small class="terms-con">
 				<a href="<?= $this->createUrl("dashboard/terms") ?>" target="_blank" class="">
 					<?php
