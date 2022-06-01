f4c44267706f6ca4cb97c41076c9de2fa3d49aa3
Tai <chayuttatat@gmail.com>
2022-06-01 17:09:59 +0700

    add page

--- admin/protected/views/coursecontrol/manageorguser.php
+++ admin/protected/views/coursecontrol/manageorguser.php
@@ -1,6 +1,6 @@
 <?php
-$titleName = 'ระบบจัดการป๊อปอัพ';
-$formNameModel = 'PopUp';
+$titleName = 'รายชื่อผู้เรียน';
+$formNameModel = 'List of learners';
 
 $this->breadcrumbs=array($titleName);
 Yii::app()->clientScript->registerScript('search', "
@@ -31,8 +31,7 @@ EOD
 , CClientScript::POS_READY);
 ?>
 
-<div class="innerLR">
-	<?php $this->widget('AdvanceSearchForm', array(
+<!-- <?php $this->widget('AdvanceSearchForm', array(
 	'data'=>$model,
 	'route' => $this->route,
 	'attributes'=>array(
@@ -40,140 +39,123 @@ EOD
 		//array('name'=>'update_date','type'=>'date'),
 		// array('name'=>'detail','type'=>'textArea'),
 	),
-	));?>
+	));?> -->
 
-	<div class="widget" style="margin-top: -1px;">
-		<div class="widget-head">
-			<h4 class="heading glyphicons show_thumbnails_with_lines"><i></i> <?php echo $titleName;?></h4>
-		</div>
-		<div class="widget-body">
-			<div class="separator bottom form-inline small">
-				<span class="pull-right">
-					<label class="strong">แสดงแถว:</label>
-					<?php echo $this->listPageShow($formNameModel);?>
-				</span>	
-			</div>
-			<div class="clear-div"></div>
-			<div class="overflow-table" style="overflow:visible !important;">
-			<?php $this->widget('AGridView', array(
-				'id'=>'popup-grid',
-				'dataProvider'=>$model->search(),
-				'filter'=>$model,
-				'rowCssClassExpression'=>'"items[]_{$data->id}"',
-				'afterAjaxUpdate'=>'function(id, data){
-						$.appendFilter("CourseOnline[news_per_page]");
-						InitialSortTable();	
-				        jQuery("#course_date").datepicker({
-						   	"dateFormat": "dd/mm/yy",
-						   	"showAnim" : "slideDown",
-					        "showOtherMonths": true,
-					        "selectOtherMonths": true,
-				            "yearRange" : "-5+10", 
-					        "changeMonth": true,
-					        "changeYear": true,
-				            "dayNamesMin" : ["อา.","จ.","อ.","พ.","พฤ.","ศ.","ส."],
-				            "monthNamesShort" : ["ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.",
-				                "ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค."],
-					   })
-					}',
-				'columns'=>array(					
-					array(
-						'header'=>'รูปภาพ',
-						'type'=>'raw',
-						'value'=> 'Controller::ImageShowIndex($data,$data->pic_file)',
-						'htmlOptions'=>array('width'=>'110')
-					),
-					array(
-						'name'=>'name',
-						'type'=>'html',
-						'value'=>'UHtml::markSearch($data,"name")'
-					),
-					// array(
-					// 	'name'=>'detail',
-					// 	'type'=>'html',
-					// 	'value'=>'UHtml::markSearch($data,"detail")'
-					// ),
-					array(
-						'name'=>'link',
-						'type'=>'html',
-						'value'=>'UHtml::markSearch($data,"link")'
-					),
-					array(
-							'name'=>'update_date',
-							'type'=>'html',
-							'value'=>$data->update_date
-						),
-					array(
-							'type'=>'raw',
-							'value'=>function($val){
-								if($val->status == 0){
-									return CHtml::link("ปิด",array("/PopUp/status","id"=>$val->id), array("class"=>"btn btn-danger"));
-								} else {
-									return CHtml::link("เปิด",array("/PopUp/status","id"=>$val->id), array("class"=>"btn btn-success"));
-								}
-							},
-							'header' => 'เปิด/ปิด การแสดงผล',
-							'htmlOptions'=>array('style'=>'text-align: center;width:100px;'),
-							'headerHtmlOptions'=>array( 'style'=>'text-align:center;width:100px;'),
-						),
-					array(
-                        'header'=>'ภาษา',
-                        'value' => function($val) {
-                           	$lang = Language::model()->findAll(array('condition' =>'active ="y"'));
-                           	$width = (count($lang)*100) + 20;
-					        foreach ($lang as $key => $value) {
-					    		$menu = Popup::model()->findByAttributes(array("lang_id" => $value->id,'parent_id'=> $val->id,'active'=>'y'));
-					    		$str = ' (เพิ่ม)';
-					    		$class = "btn btn-icon";
-					    		$link = array("/PopUp/create","lang_id"=>$value->id,"parent_id"=>$val->id);
-					    		if($menu || $key == 0){
-					    			$id = $menu ? $menu->id : $val->id;
-					    			$str = ' (แก้ไข)';
-					    			$class = "btn btn-success btn-icon";
-					    			$link = array("/PopUp/update","id"=>$id,"lang_id" =>$value->id,"parent_id"=>$val->id);
-					    		}
-					            $langStr .= CHtml::link($value->language.$str, $link, array("class"=>$class,"style" => 'width:100px;'));
-					        }
-					        return '<div class="btn-group" role="group" aria-label="Basic example">'.$langStr.'</div>';
-                    	},
-		                    'type'=>'raw',
-		                    'htmlOptions'=>array('style'=>'text-align: center','width'=>$this->getWidthColumnLang().'px;'),
-		                ),
-					array(
-							'type'=>'html',
-							'value'=>'CHtml::link("<i></i>","", array("class"=>"glyphicons move btn-action btn-inverse"))',
-							'htmlOptions'=>array('style'=>'text-align: center; width:50px;', 'class'=>'row_move'),
-							'header' => 'ย้าย',
-							'headerHtmlOptions'=>array( 'style'=>'text-align:center;'),
-						),
-					/*
-					'pic_file',
-					'create_date',
-					'create_by',
-					'update_date',
-					'update_by',
-					'active',
-					*/
-					array(            
-						'class'=>'AButtonColumn',
-						// 'visible'=>Controller::PButton( 
-						// 	array("Popup.*", "Popup.View", "Popup.Update", "Popup.Delete") 
-						// ),
-						'buttons' => array(
-							'view'=> array( 
-								// 'visible'=>'Controller::PButton( array("Popup.*", "Popup.View") )' 
-							),
-							'update'=> array( 
-								'visible'=>'Controller::PButton( array("PopUp.*", "PopUp.Update") )' 
-							),
-							'delete'=> array( 
-								'visible'=>'Controller::PButton( array("PopUp.*", "PopUp.Delete") )' 
-							),
-						),
-					),
-				),
-			)); ?>
-			</div>
-		</div>
-	</div>
+<style>
+  .w-100 {
+    width:100% !important; 
+  }
+</style>
+<div class="innerLR">
+
+  <div class="widget" style="margin-top: -1px;">
+    <div class="widget-head">
+      <h4 class="heading glyphicons show_thumbnails_with_lines"><i></i> <?php echo $titleName;?></h4>
+    </div>
+    <div class="widget-body">
+      <div class="clear-div"></div>
+      <div class="overflow-table">
+        <table class="table table-striped table-bordered table-condensed dataTable table-primary js-table-sortable ui-sortable" id="table_datatable">
+          <thead>
+          <tr>
+		  <th>ลำดับ</th>
+            <th>ชือ-นามสกุล</th>
+            <th>รหัสพนักงาน</th>
+            <th>ลำดับชั้นงาน</th>
+            <th>ตำแหน่งงาน</th>
+            <th>จัดการ</th>
+          </tr>
+          </thead>
+          <tbody>
+           <tr>
+            <td><input class="select-on-check-all" type="checkbox" value="1" name="chk_all" id="chk_all"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+           </tr>
+           <tr>
+           <td class="checkbox-column"><input class="select-on-check" value="120" id="chk_0" type="checkbox" name="chk[]"></td>
+            <td>text</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a class="btn-action glyphicons pencil btn-danger remove_2" title="ลบ" href="/lms_isuzu/admin/index.php/libraryFile/delete/120"><i></i></a></td>
+           </tr>
+           <tr>
+           <td class="checkbox-column"><input class="select-on-check" value="120" id="chk_0" type="checkbox" name="chk[]"></td>
+            <td>text</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a class="btn-action glyphicons pencil btn-danger remove_2" title="ลบ" href="/lms_isuzu/admin/index.php/libraryFile/delete/120"><i></i></a></td>
+           </tr><tr>
+           <td class="checkbox-column"><input class="select-on-check" value="120" id="chk_0" type="checkbox" name="chk[]"></td>
+            <td>text</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a class="btn-action glyphicons pencil btn-danger remove_2" title="ลบ" href="/lms_isuzu/admin/index.php/libraryFile/delete/120"><i></i></a></td>
+           </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <div class="widget" style="margin-top: -1px;">
+    <div class="widget-head">
+      <h4 class="heading glyphicons show_thumbnails_with_lines"><i></i> รายชื่อผู้เรียนทั้งหมด</h4>
+    </div>
+    <div class="widget-body">
+      <div class="clear-div"></div>
+      <div class="overflow-table">
+        <table class="table table-striped table-bordered table-condensed dataTable table-primary js-table-sortable ui-sortable" id="table_datatable">
+          <thead>
+          <tr>
+		  <th>ลำดับ</th>
+            <th>ชือ-นามสกุล</th>
+            <th>รหัสพนักงาน</th>
+            <th>ลำดับชั้นงาน</th>
+            <th>ตำแหน่งงาน</th>
+            <th>จัดการ</th>
+          </tr>
+          </thead>
+          <tbody>
+           <tr>
+            <td><input class="select-on-check-all" type="checkbox" value="1" name="chk_all" id="chk_all"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+            <td><input class="w-100" name="LibraryFile[library_name_en]" type="text" maxlength="255"></td>
+           </tr>
+           <tr>
+           <td class="checkbox-column"><input class="select-on-check" value="120" id="chk_0" type="checkbox" name="chk[]"></td>
+            <td>text</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a class="btn-action glyphicons pencil btn-danger remove_2" title="ลบ" href="/lms_isuzu/admin/index.php/libraryFile/delete/120"><i></i></a></td>
+           </tr><tr>
+           <td class="checkbox-column"><input class="select-on-check" value="120" id="chk_0" type="checkbox" name="chk[]"></td>
+            <td>text</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a class="btn-action glyphicons pencil btn-danger remove_2" title="ลบ" href="/lms_isuzu/admin/index.php/libraryFile/delete/120"><i></i></a></td>
+           </tr><tr>
+           <td class="checkbox-column"><input class="select-on-check" value="120" id="chk_0" type="checkbox" name="chk[]"></td>
+            <td>text</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td><a class="btn-action glyphicons pencil btn-danger remove_2" title="ลบ" href="/lms_isuzu/admin/index.php/libraryFile/delete/120"><i></i></a></td>
+           </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+
 </div>
