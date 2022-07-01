9acd89f3736153be71a989804826c6f391598215
p <>
2022-07-01 13:49:00 +0700

    เปลี่ยน template

--- admin/protected/views/coursequestion/import.php
+++ admin/protected/views/coursequestion/import.php
@@ -39,7 +39,7 @@ $formtext = "Import excel ข้อสอบ";
 					
 					<div class="span4">
 						<span class="label label-primary">1</span><h4>แบบฟอร์มรูปแบบนำเข้าแบบทดสอบ</label></h4>
-						<a href="<?php echo Yii::app()->getBaseUrl().'/../uploads/templete_import_questions.xls'; ?>" class="glyphicons download_alt"><i></i>Download Excel</a>
+						<a href="<?php echo Yii::app()->getBaseUrl().'/../uploads/template/templete_import_questions.xlsx'; ?>" class="glyphicons download_alt"><i></i>Download Excel</a>
 					</div>
 
 					<div class="span4">
