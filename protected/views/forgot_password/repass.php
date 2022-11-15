6371d1cb88978ec1037fbb285f151334232d7ef1
bws_keng <kengker1144@gmail.com>
2022-11-15 10:50:34 +0700

    add wording

--- protected/views/forgot_password/repass.php
+++ protected/views/forgot_password/repass.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include "themes/template2/include/css.php";
 ?>
 <div class="header-page parallax-window" data-parallax="scroll" data-image-src="<?php echo Yii::app()->theme->baseUrl; ?>/images/bg-header-page.png">
@@ -16,15 +16,15 @@ include "themes/template2/include/css.php";
 </div>
 <!-- Content -->
 <style>
-    .header-page{
+    .header-page {
         margin: 0px;
     }
 </style>
 <section class="content" id="contact-us">
     <div class="container">
         <?php
-        $form = $this->beginWidget('CActiveForm',array(
-            'action'=>Yii::app()->createUrl('/forgot_password/Repassword')
+        $form = $this->beginWidget('CActiveForm', array(
+            'action' => Yii::app()->createUrl('/forgot_password/Repassword')
         ));
         ?>
         <div class="well">
@@ -32,31 +32,36 @@ include "themes/template2/include/css.php";
                 <div class="col-sm-6 col-sm-offset-3">
                     <div class="form-group">
                         <label for="">ตั้งรหัสผ่านใหม่ / Reset Password</label><br>
-                        <?php echo $form->passwordField($model, 'password', array('class' => 'form-control input-lg' , 'required' => true)); ?>
+                        <?php echo $form->passwordField($model, 'password', array('class' => 'form-control input-lg', 'required' => true)); ?>
                         <?php echo $form->error($model, 'password'); ?>
 
-                         <label for="">ยืนตั้งรหัสผ่านใหม่ / Confirm Reset Password</label><br>
-                        <?php echo $form->passwordField($model, 'verifyPassword', array('class' => 'form-control input-lg' , 'required' => true)); ?>
+                        <label for="">ยืนตั้งรหัสผ่านใหม่ / Confirm Reset Password</label><br>
+                        <?php echo $form->passwordField($model, 'verifyPassword', array('class' => 'form-control input-lg', 'required' => true)); ?>
                         <?php echo $form->error($model, 'verifyPassword'); ?>
 
-                        <?php echo $form->hiddenField($users, 'id', array('class' => 'form-control input-lg','hidden')); ?>
+                        <?php echo $form->hiddenField($users, 'id', array('class' => 'form-control input-lg', 'hidden')); ?>
                     </div>
                 </div>
-            </div>         
+            </div>
             <div class="text-center">
                 <!--<button type="submit" class="btn btn-warning btn-lg">ส่งข้อความ</button>-->
                 <?php echo CHtml::submitButton('ยืนยัน / Confirm', array('class' => 'btn btn-warning btn-lg')); ?>
             </div>
         </div>
         <div class="row">
-			<div class="form-group" style="padding-right:15px;padding-left:15px;font-size:18px;">
-				<b>ข้อแนะนำในการตั้งรหัสผ่าน</b><br>
-				1.การตั้งรหัสผ่านควรจะมีจำนวน 6 ตัวอักษรขึ้นไป<br>
-				2.ไม่ควรใช้ข้อมูลส่วนตัว หรือข้อมูลที่คาดเดาได้ง่าย เช่น วันเดือนปีเกิดหรือเลขที่บัตรประชาชน มาตั้งรหัสผ่าน<br>
-				3.รายงานต่อเจ้าหน้าที่ดูแลระบบทันที เมื่อผู้ใช้งาน (user) คาดว่าชื่อผู้ใช้และรหัสผ่านของตนเองถูกผู้อื่นนำไปใช้งานโดยไม่ได้รับอนุญาต พร้อมกับเปลี่ยนรหัสผ่านโดยทันที<br>
-
-			</div>
-		</div>
+            <div class="form-group" style="padding-right:15px;padding-left:15px;font-size:18px;">
+                <b>ข้อแนะนำในการตั้งรหัสผ่าน</b><br>
+                1.การตั้งรหัสผ่านควรจะมีจำนวน 6 ตัวอักษรขึ้นไป<br>
+                2.ไม่ควรใช้ข้อมูลส่วนตัว หรือข้อมูลที่คาดเดาได้ง่าย เช่น วันเดือนปีเกิดหรือเลขที่บัตรประชาชน มาตั้งรหัสผ่าน<br>
+                3.รายงานต่อเจ้าหน้าที่ดูแลระบบทันที เมื่อผู้ใช้งาน (user) คาดว่าชื่อผู้ใช้และรหัสผ่านของตนเองถูกผู้อื่นนำไปใช้งานโดยไม่ได้รับอนุญาต พร้อมกับเปลี่ยนรหัสผ่านโดยทันที<br>
+                <br>
+                <b>Tips for setting up a password</b><br>
+                1. Password should be set more than 6 digits<br>
+                2. To avoid using personal data or it can be guessed easily ex. birthday or identity card number to set own password<br>
+                3. To report system administrator immediately if users found out that someone log-in the system without permission and<br>
+                please change the password immediately
+            </div>
+        </div>
         <?php $this->endWidget();
         ?>
     </div>
