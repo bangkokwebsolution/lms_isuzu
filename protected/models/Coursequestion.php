5790dcaa3e08032c87b2a7a12b5e170e5c8e4549
llOzakall <kengker1144@gmail.com>
2023-11-01 22:48:35 +0700

    fix question desc to asc

--- protected/models/Coursequestion.php
+++ protected/models/Coursequestion.php
@@ -4,12 +4,12 @@
 class Coursequestion extends AActiveRecord
 {
 	public $type = null;
-    public $excel_file = null;
+	public $excel_file = null;
 
 	public $choice_stored_ids = array();
 	public $choice_ids = array();
 
-	public static function model($className=__CLASS__)
+	public static function model($className = __CLASS__)
 	{
 		return parent::model($className);
 	}
@@ -22,13 +22,13 @@ class Coursequestion extends AActiveRecord
 	public function rules()
 	{
 		return array(
-			array('group_id', 'numerical', 'integerOnly'=>true),
+			array('group_id', 'numerical', 'integerOnly' => true),
 			//array('ques_title', 'length', 'max'=>255),
 			array('news_per_page', 'safe'),
 			array('ques_title', 'required'),
-            //array('excel_file', 'required', 'on'=>'import'),
-            array('excel_file', 'file', 'allowEmpty'=>false, 'types'=>'xls', 'on'=>'import'),
-			array('ques_id, group_id, ques_title, ques_explain , ques_type', 'safe', 'on'=>'search')
+			//array('excel_file', 'required', 'on'=>'import'),
+			array('excel_file', 'file', 'allowEmpty' => false, 'types' => 'xls', 'on' => 'import'),
+			array('ques_id, group_id, ques_title, ques_explain , ques_type', 'safe', 'on' => 'search')
 		);
 	}
 
@@ -42,65 +42,56 @@ class Coursequestion extends AActiveRecord
 		);
 	}
 
-    public function checkScopes($check = 'scopes')
-    {
-    	if ($check == 'scopes')
-    	{
-		    $checkScopes =  array(
-	        	'alias'=>'ques',
-	        	'order' => 'ques.ques_id desc',
-	            'condition'=>" ques.active='y' ",
-		    );
-    	}
-    	else
-    	{
-		    $checkScopes =  array(
-	        	'alias'=>'ques',
-	        	'order' => 'ques.ques_id desc',
-	        	'condition'=>" ques.active='y' ",
-		    );
-    	}
+	public function checkScopes($check = 'scopes')
+	{
+		if ($check == 'scopes') {
+			$checkScopes =  array(
+				'alias' => 'ques',
+				'order' => 'ques.ques_id desc',
+				'condition' => " ques.active='y' ",
+			);
+		} else {
+			$checkScopes =  array(
+				'alias' => 'ques',
+				'order' => 'ques.ques_id desc',
+				'condition' => " ques.active='y' ",
+			);
+		}
 
 		return $checkScopes;
-    }
+	}
 
 	public function scopes()
-    {
-    	//========== SET Controller loadModel() ==========//
+	{
+		//========== SET Controller loadModel() ==========//
 
-		$Access = Controller::SetAccess( array("Coursequestion.*") );
-		if($Access == true)
-		{
+		$Access = Controller::SetAccess(array("Coursequestion.*"));
+		if ($Access == true) {
 			$scopes =  array(
 				'questioncheck' => $this->checkScopes('scopes')
 			);
-		}
-		else
-		{
-			if(isset(Yii::app()->user->isSuperuser) && Yii::app()->user->isSuperuser == true)
-			{
+		} else {
+			if (isset(Yii::app()->user->isSuperuser) && Yii::app()->user->isSuperuser == true) {
 				$scopes =  array(
 					'questioncheck' => $this->checkScopes('scopes')
 				);
-			}
-			else
-			{
-			    $scopes = array(
-		            'questioncheck'=>array(
-			        	'alias'=>'ques',
-			        	'order' => 'ques.ques_id desc',
-			            'condition'=>" ques.create_by = '".Yii::app()->user->id."' AND ques.active='y' ",
-		            ),
-			    );
+			} else {
+				$scopes = array(
+					'questioncheck' => array(
+						'alias' => 'ques',
+						'order' => 'ques.ques_id desc',
+						'condition' => " ques.create_by = '" . Yii::app()->user->id . "' AND ques.active='y' ",
+					),
+				);
 			}
 		}
 
 		return $scopes;
-    }
+	}
 
 	public function defaultScope()
 	{
-	    $defaultScope =  $this->checkScopes('defaultScope');
+		$defaultScope =  $this->checkScopes('defaultScope');
 
 		return $defaultScope;
 	}
@@ -109,98 +100,90 @@ class Coursequestion extends AActiveRecord
 	{
 		return array(
 			'ques_id' => 'Ques',
-			'ques_type'=>'ประเภทข้อสอบ',
+			'ques_type' => 'ประเภทข้อสอบ',
 			'group_id' => 'ชุดข้อสอบ',
 			'ques_title' => 'โจทย์ข้อสอบ',
-            'excel_file' => 'ไฟล์ Excel Import',
+			'excel_file' => 'ไฟล์ Excel Import',
 		);
 	}
 
-   	public function beforeSave()
-    {
-    	$this->ques_title = CHtml::encode($this->ques_title);
+	public function beforeSave()
+	{
+		$this->ques_title = CHtml::encode($this->ques_title);
 
-		if(null !== Yii::app()->user && isset(Yii::app()->user->id))
-		{
+		if (null !== Yii::app()->user && isset(Yii::app()->user->id)) {
 			$id = Yii::app()->user->id;
-		}
-		else
-		{
+		} else {
 			$id = 0;
 		}
 
-		if($this->isNewRecord)
-		{
+		if ($this->isNewRecord) {
 			$this->create_by = $id;
 			$this->create_date = date("Y-m-d H:i:s");
 			$this->update_by = $id;
 			$this->update_date = date("Y-m-d H:i:s");
-		}
-		else
-		{
+		} else {
 			$this->update_by = $id;
 			$this->update_date = date("Y-m-d H:i:s");
 		}
 
-        return parent::beforeSave();
-    }
+		return parent::beforeSave();
+	}
 
-    public function afterFind()
-    {
-    	$this->ques_title = CHtml::decode($this->ques_title);
-        return parent::afterFind();
-    }
+	public function afterFind()
+	{
+		$this->ques_title = CHtml::decode($this->ques_title);
+		return parent::afterFind();
+	}
 
 
-	public function search($id=null)
+	public function search($id = null)
 	{
 		$criteria = new CDbCriteria;
-		$criteria->compare('ques_id',$this->ques_id,true);
-		$criteria->compare('ques_title',$this->ques_title,true);
+		$criteria->compare('ques_id', $this->ques_id, true);
+		$criteria->compare('ques_title', $this->ques_title, true);
 
-		if($id !== null)
-		{
-			$criteria->compare('group_id',$id,false);
+		if ($id !== null) {
+			$criteria->compare('group_id', $id, false);
 		}
 
-		$criteria->compare('ques_type',$this->ques_type,true);
+		$criteria->compare('ques_type', $this->ques_type, true);
 
-		if(!isset($_GET['Question_sort']))
-		{
+		if (!isset($_GET['Question_sort'])) {
 			$criteria->order = 'ques_id desc';
 		}
 
-		$poviderArray = array('criteria'=>$criteria);
+		$poviderArray = array('criteria' => $criteria);
 
 		// Page
-		if(isset($this->news_per_page))
-		{
-			$poviderArray['pagination'] = array( 'pageSize'=> intval($this->news_per_page) );
+		if (isset($this->news_per_page)) {
+			$poviderArray['pagination'] = array('pageSize' => intval($this->news_per_page));
 		}
 
 		return new CActiveDataProvider($this, $poviderArray);
 	}
-        
-        public static function getLimitData($id, $limit)
-    {
-        $Coursequestion = new CDbCriteria;
-        $Coursequestion->addCondition("group_id=$id");
-        $Coursequestion->addCondition("active='y'");
-        // $Coursequestion->offset    = 0;
-        $Coursequestion->limit     = $limit;
-        // $Coursequestion->order     = ' RAND() ';
 
-        return Coursequestion::model()->findAll($Coursequestion);
-    }
+	public static function getLimitData($id, $limit)
+	{
+		$Coursequestion = new CDbCriteria;
+		$Coursequestion->addCondition("group_id=$id");
+		$Coursequestion->addCondition("active='y'");
+		// $Coursequestion->offset    = 0;
+		$Coursequestion->limit     = $limit;
+		// $Coursequestion->order     = ' RAND() ';
+		$Coursequestion->order = 'ques_id ASC';
+		
+		return Coursequestion::model()->findAll($Coursequestion);
+	}
 
-    public static function getTempData($id)
-    {
-        $Question            = new CDbCriteria;
-        $Question->condition = " ques_id = '".$id."'  AND active = 'y' ";
-        $Question->offset    = 0;
-//        $Question->limit     = $limit;
-//        $Question->order     = ' RAND() ';
+	public static function getTempData($id)
+	{
+		$Question            = new CDbCriteria;
+		$Question->condition = " ques_id = '" . $id . "'  AND active = 'y' ";
+		$Question->offset    = 0;
+		//        $Question->limit     = $limit;
+		//        $Question->order     = ' RAND() ';
 
-        return Coursequestion::model()->find($Question);
-    }
+		return Coursequestion::model()->find($Question);
+	}
 }
